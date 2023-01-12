85e72ff55411a03d95c8f61175268050e77acd5f
Nikita Gupta <nikitagupta1709@gmail.com>
2023-01-12 22:25:52 +0530

    db.js done

--- stackoverflow-main/src/server.js
+++ stackoverflow-main/src/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { connection } = require("./config/db.js");
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res)=>{
+  res.send("Server started");
+})
 
 const PORT = process.env.PORT || 8080; // port at which server listening
 
-app.listen(
-  PORT,
-  console.log(`server started at port ${PORT}`)
-);
+// app.get("/user");
+// app.get("/question");
+// app.get("/answer");
+
+app.listen(PORT,()=>{
+  try {
+    connection();
+    console.log(`server started at port ${PORT}`)
+  } catch (error) {
+    console.log(error)
+  }
+});
+
