a8deedb0e7179bb602edd9d1fe50640230b9e871
Nikita Gupta <nikitagupta1709@gmail.com>
2023-01-13 19:08:58 +0530

    question all fucntions done

--- stackoverflow-main/src/controllers/question.controller.js
+++ stackoverflow-main/src/controllers/question.controller.js
@@ -17,4 +17,65 @@ const askQuestion = async(req, res) => {
     }
 }
 
-module.exports = {askQuestion}
+const allQuestions = async (req, res) =>{
+    try {
+        let allQuestions = await questionModel.find();
+        return res.send({
+            error:false,
+            data:allQuestions
+        })
+    } catch (error) {
+        return res.status(500).send({
+            error: error,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+const readQuestion = async (req, res) =>{
+    try {
+        let {_id} = req.params;
+        let question  = await questionModel.findById({_id});
+        question.count = question.count+1;
+        await questionModel.findByIdAndUpdate({_id},{count:question.count});
+        return res.send({
+            error:false,
+            data:question
+        })
+    } catch (error) {
+        return res.status(500).send({
+            error: error,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+const updateQuestion = async (req, res) =>{
+    try {
+        let {_id} = req.params;
+        let {userId, question} = req.body;
+
+        let existQuestion  = await questionModel.findById({_id});
+
+        if(userId === existQuestion.userId){
+            existQuestion.question = question
+            await questionModel.findByIdAndUpdate({_id},{question:existQuestion.question});
+            return res.send({
+                error:false,
+                data:existQuestion
+            })
+        } else{
+            return res.send({
+                error: true,
+                message: "You can not updated others question!"
+            });
+        }
+    } catch (error) {
+        return res.status(500).send({
+            error: true,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+module.exports = {askQuestion, allQuestions, readQuestion, updateQuestion}
--- stackoverflow-main/src/routes/question.route.js
+++ stackoverflow-main/src/routes/question.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { askQuestion } = require('../controllers/question.controller');
+const { askQuestion, allQuestions, readQuestion, updateQuestion } = require('../controllers/question.controller');
 
 const questionRouter = express.Router();
 
 questionRouter.post("/ask", askQuestion);
-questionRouter.get("/get");
 
+questionRouter.get("/", allQuestions);
+
+questionRouter.get("/read/:_id", readQuestion);
+
+questionRouter.patch("/update/:_id", updateQuestion);
 module.exports = questionRouter;
